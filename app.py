834e7cd4076e75e96d5228a76b853ed96016debf
obarra <obarra2023@alu.uct.cl>
2025-06-04 15:49:21 -0400

    Instalación de tailwind dentro del proyecto

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def Home():
     return render_template("./home/page.html")
 
 if __name__ == "__main__":
+    # en paralelo ejecutar:
+    # npx tailwindcss -i ./input.css -o ./static/css/style.css --watch
     app.run(debug=True, host="0.0.0.0")
