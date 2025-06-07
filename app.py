41b826b9454c81ae0f7dc4be23529912d644fa0c
obarra <obarra2023@alu.uct.cl>
2025-06-07 18:01:58 -0400

    Creación de la base de datos inicial

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
+
+import os
+from models import init_db
 from flask import Flask, render_template, redirect, url_for
-import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('/auth/login'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('/auth/login.html')
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('/auth/signup.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
+    # --- Crea la bd si no existe -----
+    db_path = 'instance/ClaveForte.db'
+    if not os.path.exists(db_path):
+        init_db(db_path)
+
+    # --- Aplicación ------------------
     app.run(debug=True)
